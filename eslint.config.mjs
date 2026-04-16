e9f17341d8769372b39536e4768d7eda62b85c3b
Huynh Nguyen Phuong Duy <huynhduylp@gmail.com>
2026-04-16 16:59:57 +0700

    feat: implement authentication system with Supabase and Prisma integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,40 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          trailingComma: "none",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          singleQuote: false,
+          printWidth: 120,
+          jsxSingleQuote: false,
+          plugins: ["prettier-plugin-tailwindcss"]
+        }
+      ]
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
-    "next-env.d.ts",
-  ]),
+    "next-env.d.ts"
+  ])
 ]);
 
 export default eslintConfig;
